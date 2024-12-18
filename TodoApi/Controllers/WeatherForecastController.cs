9d757696c14f5de347e9d51766c82bc03bb58191
vnabet <1830490+vnabet@users.noreply.github.com>
2024-12-18 15:16:38 +0000

    test de récupération de données dans la base

--- TodoApi/Controllers/WeatherForecastController.cs
+++ TodoApi/Controllers/WeatherForecastController.cs
@@ -1,6 +1,14 @@
 using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace TodoApi.Controllers;
+
+public class Test {
+    public int id {get; set;}
+    public string name {get; set;}
+}
 
 [ApiController]
 [Route("[controller]")]
@@ -18,7 +26,7 @@ public class WeatherForecastController : ControllerBase
         _logger = logger;
     }
 
-    [HttpGet(Name = "GetWeatherForecast")]
+    [HttpGet("", Name = "GetWeatherForecast")]
     public IEnumerable<WeatherForecast> Get()
     {
         return Enumerable.Range(1, 5).Select(index => new WeatherForecast
@@ -28,5 +36,25 @@ public class WeatherForecastController : ControllerBase
             Summary = Summaries[Random.Shared.Next(Summaries.Length)]
         })
         .ToArray();
+    }
+
+    [HttpGet("test", Name = "GetTest")]
+    public IEnumerable<Test> Test()
+    {
+      // Connexion à la base de données mssql VincentDB
+      using (SqlConnection connection = new SqlConnection("Server=localhost;Database=VincentDB;User Id=sa;Password=P@ssw0rd;")) {
+        connection.Open();
+        using (SqlCommand command = new SqlCommand("SELECT id,name FROM table_test", connection)) {
+          using (SqlDataReader reader = command.ExecuteReader()) {
+            while (reader.Read()) {
+              yield return new Test
+              {
+                  id = reader.IsDBNull(0) ? 0 : reader.GetInt32(0),
+                  name = reader.IsDBNull(1) ? string.Empty : reader.GetString(1)
+              };
+            }
+          }
+        }
+      }
     }
 }
