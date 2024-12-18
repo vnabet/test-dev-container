75a667c28aa0866feee1413d492e6acd7161ee87
vnabet <1830490+vnabet@users.noreply.github.com>
2024-12-18 14:35:07 +0000

    initialisation de la db

--- .devcontainer/mssql/setup.sql
+++ .devcontainer/mssql/setup.sql
@@ -1,2 +1,21 @@
 CREATE DATABASE VincentDB;
 GO
+
+create table table_test
+(
+    id   int identity
+        constraint table_test_pk
+            primary key,
+    name varchar(255) not null
+)
+
+use VincentDB
+insert into table_test (name)
+values ('tutu');
+insert into table_test (name)
+values ('tata');
+insert into table_test (name)
+values ('toto');
+insert into table_test (name)
+values ('titi');
+go
